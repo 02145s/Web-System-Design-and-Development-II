84c8c4ebd5c8e43227b2cff29dd533bcc4f116a1
wonji Ha <wj@wonjiui-MacBook-Pro.local>
2019-11-27 00:39:29 +0900

    1

---
+++ app.js
@@ -0,0 +1,77 @@
+﻿// 모듈을 추출합니다.
+var socketio = require('socket.io')(http);
+var express = require('express');
+var http = require('http');
+var ejs = require('ejs');
+var fs = require('fs');
+
+// 웹 서버를 생성합니다.
+var app = express();
+app.use(express.static('public'));
+
+// 웹 서버를 실행합니다.
+var server = http.createServer(app);
+server.listen(5000, function () {
+    console.log('server running at http://127.0.0.1:5000');
+});
+app.set('port', process.env.PORT || 8000);
+
+// 라우트를 수행합니다.
+app.get('/', function (request, response) {
+    fs.readFile('lobby.html', function (error, data) {
+        response.send(data.toString());
+    });
+});
+
+app.get('/canvas/:room', function (request, response) {
+    fs.readFile('canvas.html', 'utf8', function (error, data) {
+        response.send(ejs.render(data, {
+            room: request.params.room
+        }));
+    });
+});
+
+app.get('/room', function (request, response) {
+    var rooms = Object.keys(io.sockets.adapter.rooms).filter(function (item) {
+        return item.indexOf('/') < 0;
+    })
+    response.send(rooms);
+});
+
+// 소켓 서버를 생성합니다.
+var io = socketio.listen(server);
+io.sockets.on('connection', function (socket) {
+    var roomId = "";
+
+    socket.on('join', function (data) {
+        socket.join(data);
+        roomId = data;
+    });
+    socket.on('draw', function (data) {
+        io.sockets.in(roomId).emit('line', data);
+    });
+    socket.on('create_room', function (data) {
+        io.sockets.emit('create_room', data.toString());
+    });
+
+    //채팅 이름 설정
+    var count = 1;
+    socketio.on('connection', function(socket){
+        console.log('user connected: ', socket.id);
+        
+        
+    
+    // chat 이벤트
+    socket.on('message', function (message, name) {
+        var name= "사용자"+ count++;
+        socketio.to(socket.id).emit('change name',name);
+        var msg = name + ': ' +message;
+        console.log(msg);
+        io.sockets.emit('message', message);
+       // io.sockets.emit(msg);
+    });
+    
+});
+
+    });
+   
